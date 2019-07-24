5c8588eff88c72e2ee2492350c52a884298cb0fa
Moustikitos <moustikitos@gmail.com>
2019-07-24 13:21:39 +0200

    Update ecschnorr.py
    added sign_rfc6979

--- src/ecpy/ecschnorr.py
+++ src/ecpy/ecschnorr.py
@@ -158,7 +158,23 @@ class ECSchnorr:
             k (ecpy.keys.ECPrivateKey)     : random to use for signing
         """
         return self._do_sign(msg, pv_key,k)
-            
+
+    def sign_rfc6979(self, msg, pv_key, hasher, canonical=False):
+        """ Signs a message hash according to RFC6979 
+        Args:
+            msg (bytes)                    : the message hash to sign
+            pv_key (ecpy.keys.ECPrivateKey): key to use for signing
+            hasher (hashlib)               : hasher conform to hashlib interface
+        """
+        field = pv_key.curve.field
+        V = None
+        for i in range(1, self.maxtries):
+            k,V = ecrand.rnd_rfc6979(msg, pv_key.d, field, hasher, V)
+            sig = self._do_sign(msg, pv_key, k)
+            if sig:
+                return sig
+            return None
+
     def _do_sign(self, msg, pv_key, k):
         if (pv_key.curve == None):
             raise ECPyException('private key haz no curve')
